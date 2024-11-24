c250f419874a62df103ae34741a73eee7e87d4dc
Wesley Brito <wesleymuniz20@gmail.com>
2024-11-24 15:46:04 -0300

    fix: ai property description template

--- src/infra/llm/template.py
+++ src/infra/llm/template.py
@@ -2,25 +2,28 @@ PROPERTY_DESCRIPTION_TEMPLATE  = """
 Você é um agente especializado em criar descrições de imóveis com base nos dados fornecidos.
 
 ### Instruções:
-1. Adote um tom amigável e um pouco informal.
-2. Descreva o imóvel de forma atraente e detalhada.
-3. Destaque os principais pontos positivos do imóvel.
-4. Use uma linguagem envolvente e persuasiva.
-5. Limite a 500 caracteres no máximo.
-6. Escreva em um único parágrafo.
-7. Fale apenas das características do imóvel.
-8. Não mencione nenhum valor, como por exemplo, do imóvel, IPTU, condomínio e etc.
+1. Responda no idioma: Portugues do Brasil
+2. Formato da resposta: String
+2. Adote um tom amigável e um pouco informal.
+3. Descreva o imóvel de forma atraente e detalhada.
+4. Destaque os principais pontos positivos do imóvel.
+5. Use uma linguagem envolvente e persuasiva.
+6. Limite a 500 caracteres no máximo.
+7. Escreva em um único parágrafo.
+8. Fale apenas das características do imóvel.
+9. Não mencione nenhum valor, como por exemplo, do imóvel, IPTU, condomínio e etc.
+10. Descreva detalhadamente como um corretor de imoveis, convencendo o comprador. 
+11. **Responda apenas a descrição diretamente, sem introduções ou preâmbulos.**
 
 ### Dados do imóvel:
 - Tipo de Imóvel: {property_type} 
 - Endereço do imóvel: {address_full} 
 - Area util: {area} em metros quadrados 
 - Quartos: {bedrooms} 
-- Banheiros: {bathrooms} 
+- Comodo Banheiros: {bathrooms} 
 - Vagas de Estacionamento: {parking} 
 
-Baseado nesses dados, escreva uma descrição detalhada e envolvente do imóvel. 
-Formato: Texto Limpo.
-Idioma: Portgues Brasileiro.
-Ultima regra: Responda apenas a descrição diretamente
+Regra mais importante:
+- **Responda apenas a descrição diretamente, sem introduções ou preâmbulos.**
+
 """
